4fa262ff4ff1bc237587831606bd455dfebaffff
PC Casa <guilhermegalvao925@gmail.com>
2024-02-18 21:37:07 -0400

    Primeira atividade Finalizada

--- Atividade Prática I/Atividade1.js
+++ Atividade Prática I/Atividade1.js
@@ -5,22 +5,15 @@ Considerar todas as variáveis do tipo de dado real, que serão digitadas pelo u
 Preço * Quantidade = Valor total 
 Valor total - Desconto = Valor final*/
 
-console.log("Olá Mundo !");
-
-preco = Number;
-quantidade = Number;
-valor_total = Number;
-desconto = Number;
 
 
-console.log("Informe o valor do Produto: \n");
-preco = 175.00;
-console.log("Informe a quantidade a ser Vendida: \n");
-quantidade = 2;
-valor_total = preco * quantidade;
-console.log("Este é o valor do produto, sem desconto: "+valor_total+"\n");
-console.log("Informe o valor do desconto, caso não tenha desconto informe 0: \n");
-desconto = 50;
-valor_total = valor_total - desconto; 
-console.log("Este e o valor final do produto: "+valor_total+"\n");
 
+var readlineSync = require('readline-sync')
+
+var preco = readlineSync.question('\nInforme o valor do Produto: ');
+var quantidade = readlineSync.question('\nInforme a quantidade a ser vendida: ')
+var valor_total = preco * quantidade;
+console.log('\nEste é o valor do produto, sem desconto: '+valor_total+'\n');
+var desconto = readlineSync.question('\nInforme o valor do desconto, caso nao tenha desconto informe o valor zero (0): ');
+valor_total = valor_total -  desconto;
+console.log('\nEste e o valor final do produto: '+valor_total+'\n');
